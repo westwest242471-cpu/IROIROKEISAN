351163859c6a3aa419ca500d4e3172d0b17c8b5a
westwest242471-cpu <westwest242471@gmail.com>
2026-03-13 20:30:49 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'genba-calc-v2';
+const CACHE_NAME = 'genba-calc-v3';
 const urlsToCache = [
   './',
   './index.html',
@@ -39,6 +39,7 @@ self.addEventListener('activate', (event) => {
     })
   );
 });
+
 
 
 
